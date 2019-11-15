257f8dfdf9d6a2190d8f7f9ba0af7f9b55395aed
Hilary Zen <hzen00@stuy.edu>
2019-11-15 02:16:59 -0500

    Finished

--- randfile.c
+++ randfile.c
@@ -4,8 +4,33 @@
 #include <unistd.h>
 
 int main() {
+  printf("Generating random numbers:\n");
   int fd;
-  fd = open("/dev/random", 0_RDONLY);
+  fd = open("/dev/urandom", O_RDONLY);
+  int buff[10];
+  read(fd, buff, 10 * sizeof(int));
+  close(fd);
+  int x = 0;
+  for (x = 0; x < 10; x++) {
+    printf("Random %d: %d\n", x, buff[x]);
+  }
+
+  printf("\nWriting numbers to file:\n");
+  int fd_write;
+  fd_write = open("numbers", O_CREAT | O_TRUNC | O_WRONLY, 0644);
+  write(fd_write, buff, 10 * sizeof(int));
+  close(fd_write);
+
+  printf("\nChecking random numbers:\n");
+  int fd2;
+  fd2 = open("numbers", O_RDONLY);
+  int buff2[10];
+  read(fd2, buff2, 10 * sizeof(int));
+  close(fd2);
+  int y = 0;
+  for (y = 0; y < 10; y++) {
+    printf("Random %d: %d\n", y, buff2[y]);
+  }
 
   return 0;
 }
